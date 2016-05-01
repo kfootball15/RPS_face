77d75da8b9e70cf5205ef86c1b2f87b1874a8ed8
Jeff <fenster.js@gmail.com>
2016-05-01 19:41:59 -0400

    Working Version with NO https

--- browser/js/dogTags/dogTags.controller.js
+++ browser/js/dogTags/dogTags.controller.js
@@ -18,22 +18,49 @@ app.controller('dogTagsCtrl', function($scope, $stateParams, DogTagsFactory, log
     //     theirCtx.drawImage(theirImage, 0, 0)
     // }
 
-    $scope.createCanvas = function(img){
-        var theCanvas = document.getElementById('theCanvas')
-        // var newCanvas = document.createElement('canvas')
-        // console.log(newCanvas)
+    // $scope.createCanvas = function(img){
+    //     var theCanvas = document.getElementById('theCanvas')
+    //     // var newCanvas = document.createElement('canvas')
+    //     // console.log(newCanvas)
 
-        // // var theirCanvas = document.getElementById('theirCanvas');
-        var newCtx = theCanvas.getContext('2d');
-        var newImage = new Image();
-        newImage.src = img;
-        var newContent = newCtx.drawImage(newImage, 0, 0)
-        // newCanvas.appendChild(newContent)
-        theCanvas.appendChild(newContent)
+    //     // // var theirCanvas = document.getElementById('theirCanvas');
+    //     var newCtx = theCanvas.getContext('2d');
+    //     var newImage = new Image();
+    //     newImage.src = img;
+    //     var newContent = newCtx.drawImage(newImage, 0, 0)
+    //     // newCanvas.appendChild(newContent)
+    //     theCanvas.appendChild(newContent)
+    // }
+
+
+
+
+    // var count = 0;
+    var dogTagDiv = document.getElementById('dogTagDiv')
+    for (var i = 0; i < $scope.allDogTags.length; i++) {
+        var canv = document.createElement("canvas");
+        canv.setAttribute('width', 400);
+        canv.setAttribute('height', 300);
+        canv.setAttribute('id', i);
+        canv.setAttribute('class', 'dogTagClass')
+        dogTagDiv.appendChild(canv);
     }
 
+    for (var i = 0; i < $scope.allDogTags.length; i++) {
 
+        // $scope.allDogTags[i]
+        var tempCanvas = document.getElementById(i);
+        console.log("tempCanvas", tempCanvas)
 
+        // var tempCanvas = document.getElementById('tempCanvas');
+        var theirCtx = tempCanvas.getContext('2d');
+        var theirImage = new Image();
+        theirImage.src = $scope.allDogTags[i].image
+        theirCtx.drawImage(theirImage, 0, 0)
+        // // var newContent =
+        // // newCanvas.appendChild(newContent)
+        // document.appendChild(newCtx.drawImage(newImage, 0, 0))
+    }
 
 
 
